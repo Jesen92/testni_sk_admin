b711bed4fe4d1c290ceefd7d6648eb2a7cb1da03
Hrvoje Jesenović <hjesenovic@gmail.com>
2015-08-01 10:57:35 +0200

    date format

---
+++ config/initializers/date_format.rb
@@ -0,0 +1,7 @@
+ # Date
+  # ----------------------------
+  Date::DATE_FORMATS[:default] = "%d.%m.%Y."  # 03.11.2013
+
+  DateTime::DATE_FORMATS[:default] = "%d.%m.%Y. %H:%M"  
+
+  Time::DATE_FORMATS[:default] = "%H:%M"                     # 14:22
