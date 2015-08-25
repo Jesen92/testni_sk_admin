1a27fb8aca3d7f83dabe84a4c18f683f4f13bcac
Hrvoje Jesenović <hjesenovic@gmail.com>
2015-08-25 17:51:34 +0200

    nesto

--- app/controllers/androids_controller.rb
+++ app/controllers/androids_controller.rb
@@ -4,7 +4,7 @@ class AndroidsController < ApplicationController
 	skip_before_filter  :verify_authenticity_token
 
   def get_json
-  	response = HTTParty.get('http://localhost:3000/androids/get_json')
+  	response = HTTParty.get('http://skola-str.herokuapp.com/androids/get_json')
 	body = JSON.parse(response.body)
 	@ucenik = NoviUcenik.new(ucenik_params)
 	@ucenik.name = body["name"]
