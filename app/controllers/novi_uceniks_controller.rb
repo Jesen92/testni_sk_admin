a820dc0ba11b274d100a82f9e49dc7e614b261e7
Hrvoje Jesenović <hjesenovic@gmail.com>
2015-08-26 23:04:30 +0200

    nesto

--- app/controllers/novi_uceniks_controller.rb
+++ app/controllers/novi_uceniks_controller.rb
@@ -5,13 +5,13 @@ class NoviUceniksController < ApplicationController
   end
 
   def create
-  	@ucenik = NoviUcenik.new({:name => params[:name], :mjesto => params[:mjesto], :jezik => params[:jezik], :parents_name => params[:parents_name], :tel => params[:tel], :email => params[:email], :comment => params[:comment], :created_at => Time.now})
+  	@ucenik = NoviUcenik.new({:name => params[:name], :mjesto => params[:mjesto], :jezik => params[:jezik], :vrsta_tecaja => params[:vrsta_tecaja],:parents_name => params[:parents_name], :tel => params[:tel], :email => params[:email], :comment => params[:comment], :created_at => Time.now})
 
   	@ucenik.save
   end
 
   private
   	def user_params
-  		params.require(:novi_ucenik).permit(:name, :mjesto, :jezik, :parents_name, :tel, :email, :comment, :created_at)
+  		params.require(:novi_ucenik).permit(:name, :mjesto, :jezik, :vrsta_tecaja, :parents_name, :tel, :email, :comment, :created_at)
   	end
 end
