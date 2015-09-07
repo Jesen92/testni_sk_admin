8bca756bc656293b5565703217cd19b52af1cdd4
Hrvoje Jesenović <hjesenovic@gmail.com>
2015-09-07 19:26:07 +0200

    nesto

--- app/controllers/novi_uceniks_controller.rb
+++ app/controllers/novi_uceniks_controller.rb
@@ -5,11 +5,10 @@ class NoviUceniksController < ApplicationController
   end
 
   def create
-    if NoviUcenik.where({name: params[:name], mjesto: params[:mjesto], jezik: params[:jezik], vrsta_tecaja: params[:vrsta_tecaja], parents_name: params[:parents_name], tel: params[:tel], email: params[:email], comment: params[:comment]) != nil
-  	@ucenik = NoviUcenik.new({:name => params[:name], :mjesto => params[:mjesto], :jezik => params[:jezik], :vrsta_tecaja => params[:vrsta_tecaja],:parents_name => params[:parents_name], :tel => params[:tel], :email => params[:email], :comment => params[:comment], :created_at => Time.now})
+   	@ucenik = NoviUcenik.new({:name => params[:name], :mjesto => params[:mjesto], :jezik => params[:jezik], :vrsta_tecaja => params[:vrsta_tecaja],:parents_name => params[:parents_name], :tel => params[:tel], :email => params[:email], :comment => params[:comment], :created_at => Time.now})
 
   	@ucenik.save
-    end
+
   end
 
   private
